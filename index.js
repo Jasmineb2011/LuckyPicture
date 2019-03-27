e1208a4ef7e57ad98925ada74ced84aa5c61865a
Jasmineb2011 <Teddybear13.TB@gmail.com>
2019-03-27 12:42:11 +0200

    worked on css and js url

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function changeBackground() {
   var picId = document.getElementById('nPic').value;
-  document.body.style.backgroundImage = "url(https:picsum.photos/200/200?image="+picId+")";
+  document.body.style.backgroundImage = "url(https:picsum.photos/2000/1000?image="+picId+")";
 }
-
-// fetch('https://picsum.photos/list')
-// .then(function(response) {
-//   return response.json();
-// })
-// .then(function(data) {
-//   var idURL = data[picId].post_url;
-// })
+function changeBlur() {
+  var picId = document.getElementById('nPic').value;
+  document.body.style.backgroundImage = "url(https:picsum.photos/2000/1000?image="+picId+"&blur)";
+}
+function changeGray() {
+  var picId = document.getElementById('nPic').value;
+  document.body.style.backgroundImage = "url(https://picsum.photos/g/2000/1000?image="+picId+")";
+}

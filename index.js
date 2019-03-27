b68ce57958c98184932dfb06d16ae391d7c72fd0
Noorb7188 <noorb7188@gmail.com>
2019-03-27 09:22:54 +0200

    first JS function

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-fetch('https://picsum.photos/list')
-.then(function(response) {
-  return response.json();
-})
-.then(function(data) {
-  var picId = document.getElementById('picId').value;
-  picId = data.post_url;
-})
+function changeBackground() {
+  var picId = document.getElementById('nPic').value;
+  document.body.style.backgroundImage = "url(https:picsum.photos/200/200?image="+picId+")";
+}
+
+// fetch('https://picsum.photos/list')
+// .then(function(response) {
+//   return response.json();
+// })
+// .then(function(data) {
+//   var idURL = data[picId].post_url;
+// })

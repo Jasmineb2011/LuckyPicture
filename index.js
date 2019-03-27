3b56bdcc902d4095b88403a58f722bc045822803
enasskmal <enasskmal@gmail.com>
2019-03-27 12:58:04 +0200

    worked for tha dawnload function

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ function changeBackground() {
   document.body.style.backgroundImage = "url(https:picsum.photos/200/200?image="+picId+")";
 }
 
-// fetch('https://picsum.photos/list')
-// .then(function(response) {
-//   return response.json();
-// })
-// .then(function(data) {
-//   var idURL = data[picId].post_url;
-// })
+function downloadPic(event){
+  event.preventDefault();
+  fetch('https://picsum.photos/list')
+    .then(function(response) {
+      return response.json();
+    })
+     .then(function(data) {
+       var picId = document.getElementById('nPic').value;
+       console.log(data[picId].post_url);
+       window.open(data[picId].post_url, '_blank');
+      })
+      .catch(function(error){
+        console.log(error);
+      })
+ }
+
+ document.getElementById("download").addEventListener("click", downloadPic);

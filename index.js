e58af4e28a1ea2daa86b39ec17e91a0453c32a11
Noorb7188 <noorb7188@gmail.com>
2019-03-28 11:19:44 +0200

    added more DOM manioulation

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function changeBackground() {
   var picIdNum = document.getElementById('picId').value;
   document.body.style.backgroundImage = "url(https://picsum.photos/2000/1000?image="+picIdNum+")";
-}
+    fetch('https://picsum.photos/list')
+      .then(function(response) {
+        return response.json();
+        })
+      .then(function(data) {
+        var span = document.getElementById("authorData");
+        var link = document.createElement("a");
+        link.textContent = "Check the Author's other pictures";
+        link.href = data[picIdNum].author_url;
+        link.target = "_blank";
+        span.appendChild(link);
+        })
+      .catch(function(error){
+        console.log(error);
+        })
+   }
+   window.open(data[picIdNum].post_url, '_blank');
+
 function changeBlur() {
   var picIdNum = document.getElementById('picId').value;
   document.body.style.backgroundImage = "url(https://picsum.photos/2000/1000?image="+picIdNum+"&blur)";
